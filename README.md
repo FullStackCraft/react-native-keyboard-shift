7a5784eea9454034e88cb4b5158c2a9a85af3dda
Chris Frewin <frewin.christopher@gmail.com>
2021-10-03 12:52:24 +0200

    initial commit! will it blend? (work?) :)

---
+++ README.md
@@ -0,0 +1,72 @@
+# react-native-keyboard-shift
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/princefishthrower/react-native-keyboard-shift/blob/master/LICENSE) [![npm version](https://img.shields.io/npm/v/react-native-keyboard-shift.svg?style=flat)](https://www.npmjs.com/package/react-native-keyboard-shift) ![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)
+
+# Example
+
+_Snack coming soon_
+
+Until then:
+
+1. Clone this repo:
+
+```bash
+git clone https://github.com/FullStackCraft/react-native-keyboard-shift
+```
+
+2. Move into the example folder:
+
+```
+cd react-native-keyboard-shift/example
+```
+
+3. Install dependencies:
+
+```
+npm install
+```
+
+4. Start Android
+
+```
+npm run android
+```
+
+or iOS:
+
+```
+npm run ios
+```
+
+Enjoy the juicy keyboard shifty-ness!
+
+# Usage In Your Project
+
+Install and save this package as a dependency:
+
+```bash
+npm install --save @fullstackcraft/react-native-keyboard-shift
+```
+
+Import and use the KeyboardShift component:
+
+```tsx
+import '@fullstackcraft/react-native-keyboard-shift'
+// other imports
+
+export default function YourCoolKeyboardScreen () {
+
+    // Other logic, variables, etc.
+
+    return (
+        <KeyboardShift>
+            {/* Screen components */}
+        </KeyboardShift>
+    )
+}
+```
+
+# ANOTHER Keyboard Shifting Component?!
+
+See the original [blog post](https://chrisfrewin.medium.com/a-keyboard-avoiding-view-for-react-native-in-2021-196701ff0608) that led to the creation of this library for more information.
+
---
+++ src/components/KeyboardShift.tsx
@@ -0,0 +1,83 @@
+import React, { PropsWithChildren, useEffect, useState } from 'react';
+import { Platform, Animated, Dimensions, Keyboard, KeyboardAvoidingView, StyleSheet, TextInput } from 'react-native';
+import {useHeaderHeight} from '@react-navigation/elements';
+import { useKeyboard } from '@react-native-community/hooks';
+
+export default function KeyboardShift (props: PropsWithChildren<{}>) {
+  const [shift] = useState(new Animated.Value(0))
+  const keyboard = useKeyboard()
+
+  // On mount, add keyboard show and hide listeners
+  // On unmount, remove them
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', handleKeyboardDidShow);
+    Keyboard.addListener('keyboardDidHide', handleKeyboardDidHide);
+    return () => {
+      Keyboard.removeAllListeners('keyboardDidShow');
+      Keyboard.removeAllListeners('keyboardDidHide');
+    }
+  }, [])
+
+  const handleKeyboardDidShow = () => {
+    const { height: windowHeight } = Dimensions.get('window');
+    const keyboardHeight = keyboard.keyboardHeight;
+    const currentlyFocusedInputRef = TextInput.State.currentlyFocusedInput();
+    currentlyFocusedInputRef.measure((_x, _y, _width, height, _pageX, pageY) => {
+      const fieldHeight = height;
+      const fieldTop = pageY;
+      const gap = (windowHeight - keyboardHeight) - (fieldTop + fieldHeight);
+      if (gap >= 0) {
+        return;
+      }
+      Animated.timing(
+        shift,
+        {
+          toValue: gap,
+          duration: 1000,
+          useNativeDriver: true,
+        }
+      ).start();
+    })
+  }
+
+  const handleKeyboardDidHide = () => {
+    Animated.timing(
+      shift,
+      {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }
+    ).start();
+  }
+
+  const { children } = props;
+
+  // Android: we need an animated view since the keyboard style can vary widely
+  // And React Native's KeyboardAvoidingView isn't always reliable
+  if (Platform.OS === 'android') {
+    return (
+      <Animated.View style={[styles.container, { transform: [{translateY: shift}] }]}>
+        {children}
+      </Animated.View>
+    );
+  }
+
+  // iOS: React Native's KeyboardAvoidingView with header offset and
+  // behavior 'padding' works fine on all ios devices (and keyboard types)
+  const headerHeight = useHeaderHeight();
+  return (
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={headerHeight}
+      style={styles.container}
+      behavior={'padding'}>
+      {children}
+    </KeyboardAvoidingView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
