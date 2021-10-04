3b60793d7ba2afd1e4a10b94edaf5b38de4a06c2
Chris Frewin <frewin.christopher@gmail.com>
2021-10-04 19:46:17 +0200

    more tooling

--- src/components/KeyboardShift.tsx
+++ src/components/KeyboardShift.tsx
@@ -1,11 +1,11 @@
 import React, { PropsWithChildren, useEffect, useState } from 'react';
 import { Platform, Animated, Dimensions, Keyboard, KeyboardAvoidingView, StyleSheet, TextInput } from 'react-native';
-import {useHeaderHeight} from '@react-navigation/elements';
+import { useHeaderHeight } from '@react-navigation/elements';
 import { useKeyboard } from '@react-native-community/hooks';
 
-export default function KeyboardShift (props: PropsWithChildren<{}>) {
-  const [shift] = useState(new Animated.Value(0))
-  const keyboard = useKeyboard()
+export default function KeyboardShift(props: PropsWithChildren<{}>) {
+  const [shift] = useState(new Animated.Value(0));
+  const keyboard = useKeyboard();
 
   // On mount, add keyboard show and hide listeners
   // On unmount, remove them
@@ -15,8 +15,8 @@ export default function KeyboardShift (props: PropsWithChildren<{}>) {
     return () => {
       Keyboard.removeAllListeners('keyboardDidShow');
       Keyboard.removeAllListeners('keyboardDidHide');
-    }
-  }, [])
+    };
+  }, []);
 
   const handleKeyboardDidShow = () => {
     const { height: windowHeight } = Dimensions.get('window');
@@ -25,59 +25,46 @@ export default function KeyboardShift (props: PropsWithChildren<{}>) {
     currentlyFocusedInputRef.measure((_x, _y, _width, height, _pageX, pageY) => {
       const fieldHeight = height;
       const fieldTop = pageY;
-      const gap = (windowHeight - keyboardHeight) - (fieldTop + fieldHeight);
+      const gap = windowHeight - keyboardHeight - (fieldTop + fieldHeight);
       if (gap >= 0) {
         return;
       }
-      Animated.timing(
-        shift,
-        {
-          toValue: gap,
-          duration: 1000,
-          useNativeDriver: true,
-        }
-      ).start();
-    })
-  }
-
-  const handleKeyboardDidHide = () => {
-    Animated.timing(
-      shift,
-      {
-        toValue: 0,
+      Animated.timing(shift, {
+        toValue: gap,
         duration: 1000,
         useNativeDriver: true,
-      }
-    ).start();
-  }
+      }).start();
+    });
+  };
+
+  const handleKeyboardDidHide = () => {
+    Animated.timing(shift, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
 
   const { children } = props;
 
   // Android: we need an animated view since the keyboard style can vary widely
   // And React Native's KeyboardAvoidingView isn't always reliable
   if (Platform.OS === 'android') {
-    return (
-      <Animated.View style={[styles.container, { transform: [{translateY: shift}] }]}>
-        {children}
-      </Animated.View>
-    );
+    return <Animated.View style={[styles.container, { transform: [{ translateY: shift }] }]}>{children}</Animated.View>;
   }
 
   // iOS: React Native's KeyboardAvoidingView with header offset and
   // behavior 'padding' works fine on all ios devices (and keyboard types)
   const headerHeight = useHeaderHeight();
   return (
-    <KeyboardAvoidingView
-      keyboardVerticalOffset={headerHeight}
-      style={styles.container}
-      behavior={'padding'}>
+    <KeyboardAvoidingView keyboardVerticalOffset={headerHeight} style={styles.container} behavior={'padding'}>
       {children}
     </KeyboardAvoidingView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
